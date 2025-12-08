73ff1ef5c0bf4a66623c87b7fefb67a1317ac2ed
Pedro S.L <pl2828280@gmail.com>
2025-12-08 13:41:58 -0300

    fix: remove arquivos desnecessários

--- src/utils/getUserId.js
+++
@@ -1,9 +0,0 @@
-import jwtDecode from "jwt-decode";
-
-export function getUserId() {
-  const token = localStorage.getItem("token");
-  if (!token) return null;
-
-  const decoded = jwtDecode(token);
-  return decoded.sub; // ID do usuário
-}

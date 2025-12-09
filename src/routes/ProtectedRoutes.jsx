0d747f29220f27130650ff249d4965b670d7d138
BiancaReisMoraes <biancadosreisprofissional@gmail.com>
2025-12-09 18:01:04 -0300

    fix: arruma css das telas

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -3,9 +3,7 @@ import { Navigate } from "react-router-dom";
 export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
 
-  // Verifica se o token existe e não está vazio
   if (!token || token.trim() === "") {
-    // Limpa qualquer dado inválido
     localStorage.removeItem("token");
     localStorage.removeItem("id");
     localStorage.removeItem("userName");
